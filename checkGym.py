5f758083ab34027b828fc34299913193fbe6a5f9
kwonjun <116526991+jun060703@users.noreply.github.com>
2024-01-08 12:44:04 +0900

    전체

---
+++ checkGym.py
@@ -0,0 +1,46 @@
+import cv2
+import time
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+face_cascade = cv2.CascadeClassifier('C:/python2/dkdk/ddd.xml')
+
+cam = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            break
+
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray_frame)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        if not ret:
+            break
+
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        time.sleep(0)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    try:
+        app.run(host='0.0.0.0', port=5050)
+    finally:
+        cam.release()
+        cv2.destroyAllWindows()
