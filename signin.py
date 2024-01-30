cf44cde157752198f768bb930cb69a3c61559d86
kwonjun <116526991+jun060703@users.noreply.github.com>
2024-01-30 14:14:56 +0900

    Update signin.py

--- signin.py
+++ signin.py
@@ -21,32 +21,7 @@ from datetime import datetime
 from werkzeug.utils import secure_filename
 
 # Firebase 관련 설정
-cred = credentials.Certificate({
-  "type": "service_account",
-  "project_id": "checkgym-322d2",
-  "private_key_id": "36bf14196423b556d36527b1f7cc2d87cc986042",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDZFy/Wz8ess1Sw\nqd551y2S1s2z2Tm3Yit2EjhBxh6IGUHBdsBZrv2rD6HGfAxn1axUdRH85x+Cnwwb\n7HiAv9dS9O1RVoqXjy3etFxv+tzI3H9+lyShZDjT2k34nNzXAKKt4w7AV3qYV+qW\nGmTmQSm6IOT+41xHKLp69hQRd0eTIS/TI7YkEKO9IDLRO5F1I1Xw9xvNhYUWDaDO\nmoFOBsNcaRQRnjPDc/BuXdWm0OAc5gL9sdW/lNboP54+URjNnTaPhPC/MZKqXlHD\nm5YnhRIUOI6qIH0spYoEs8RYjbTvq23b7S+OmRaUWULSU3nx0QP7OaPqVhfH8ZQ5\n0tCnD5LrAgMBAAECggEAFQt+ybyh+lRc9QG8Q+RGOLvpSQwMrYH7/WaawyJcbb+O\np+osXDiIj5kX8VCvnmLdlK2crTJ10+9sxX0EP+1ZB5bsaQj4rbFlnJDjfSXwJupC\n02TKwKwhWvg775ciGX2oR5WBWPs7mTuJZ2+7l4Hg2t67FEwgjMCeabK9Rn9hSU16\nhMC4nk0YWm67Y6+QGHh6YchC/3lgpfsSSSw7ivCO4rGldow8loOO0bwV7ykquc04\nQuP5AnI7WvYtZYLnh4zyc6aiRZjOL4BGZFqdP1LcgTzoj1GCdUsMnrqMepYxzOSE\nE/Sla8mtEMqNmnAZQGueiDm+a8gUZXkOwEzJ7/n4sQKBgQD5qE+vB/q6OMyCYR4H\nc2CsmFKvDZckoAYvLlFrz6j60zzZscuKdOEfTF+3eD7T1CfLm4ZSv5zsHpIYuVhg\n2QCzuEHQHJoLYQiZPRFFKi2yxB7tY61wnh6cnf8Nq7A29Mgft79dgM3ZoaRbwEHc\nWEZwDNrUnfuN1iW7+kaWfNFJHQKBgQDemxJIv7GFFuQpKPcK1emuRJDThyH/VBR9\nE3meRVp4WO07rApNhIe9FBQl8uf6FI47Hk3pSHcDtM7Sn6quq15kjCQ3br7lpOoC\naq4plX1SF51SMRtHua2lXQEUZzmbMK008juYG9pSfrSlG8zFyvzxrcqD92RxtIWk\ndIeTYPeVpwKBgQC19tY1VdSKXJG6ybxQY+jng5JoYrhyCmzXgKP5t36LauFkLjGT\ndgMLg+gT2oG4dV6YCYBK2bwvYUzM1nKzDBd15muZmu8wMgZJYngu+EIclNOR5N/q\nQGVBc+sNdMDYHWurqorBRPtEj4szEJswVerpCoWJCKjrxVT1gMJoNwt3hQKBgQDX\nS9IGTGiMGRS4/mbYswWcx7Hu896czRw30FMrEVar2Q4xTXZ1fL2v0LCf92wHgkQB\nx5CBFNYO3pu+ODDjWNkllCke99xnzPWLOtSRYYTglfhRWy9QUrQwoF//9MpRX2XW\nNIYQ2rdwXB2pJtcJYgb8VCLvOaGLG5a59P5/OySaNwKBgGCmqUJSDbw/AXRgbJYY\n6a/3cjyZUMX9nuIrkZ1Ibmyf+54w3NO3t9ceK6eiS2+Acz5kXOt5OfAcUFjbtsUU\nDEhK6lwkdN8NNLBeSZ13LRE9gOnNoiJLjEvQaVbJG1kxnMQV9oLJCRegkoD8MzZs\nkXVaFvJlg4UrfZShoBa35eZe\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-bmi70@checkgym-322d2.iam.gserviceaccount.com",
-  "client_id": "116016558862174674055",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-bmi70%40checkgym-322d2.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-})
-cred2 = credentials.Certificate({
-  "type": "service_account",
-  "project_id": "checkgym-322d2",
-  "private_key_id": "2fa2dd1f7820cfcb9eb443a4d256e23422c11325",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDHaQu+52gvsEDn\nwDAL2O9c5eQThRYkqG+GoJcdGZaVZYSpJX9L0bdoliwCqlVjPPUXcx7wHhvzonao\n4JqovhAARCbwHo5mrepmSH5xckVJM314QQY4f36Tzy0ZW1kTBb3GC+Bd15Gn5EEh\nyRZCKX1B0nOPC5W+Vi0HmNhGPKrXduMGlfCShrzM0965QmNURzGOes0XCjkKD6VU\n/MvMohW6dR8xQrfaR83vMlvCNeoPuCkJQUBY+5dovoDa/6+Mq8ODd6usL8C/vYUv\n40mKgu8SAsIsmku5MVvnTWYj7P6ZKutrayx/ZukocF1DSTESn56VGWS3XUYvwxN0\n6wr3hnbbAgMBAAECggEAQvf2z16UwUs+iPYkzJ3GyVqVba7I4HfGuJ5PEaFJzpOA\n6XNj3FkqcM/aKOz+by0GHKF3VumttnUtx4pitl/aLNS2hPamGTq4GTCocj4PocVO\nkme4CJpcrpQpz7W7ZOWNNalAaROf4ZQJAakwNfkMDWJ5l6Uq3XjAd6gvner67w/w\n6p1UTn4C9fXmz92dhduWwhlFi+GTxT49KPlaNIwPnskoA6kcyL7hy2/t7QfYi56t\nX41YoN6hiiCkRLYubanaf4N5PQ8goiofheN19TwE5f5hMPaoraGSuqqwuCJkiR1v\nf2DH8hcXv5FdkSLffPgdm6/dg0gSh8wso0J9z70pYQKBgQDklO1bMeODJoPK3Cph\njoVpqUCgklyBJ9XwMfpLkkTbFLUbvf3zMJeeEvMXD8X9/7il+zTiI4mulNfKXfpG\njhBZi0PPoleXcBM4lBdKuIYSsGR83S7JlVhpIDf6igoCGpsoDAZk4+EduyD3Erz4\npqQODNqb7rZW3uNQVhbNoBvs+wKBgQDfVFm60KZHamm6M0nmnpNS7DMrjpHRmkkU\nWrBYvaHkjFmMrFMfNuiBZG+Xo/znm6ujbxGPLhXKUlB41pDK7SseBjUAlXCBpdg8\nLGFj3v9mmK2C71XO9i+KHDnc1ZJTqNj/pID83nFvlAeRecSJtBWK0tHlKxrGBrLZ\nOIJye323oQKBgBAQn3XvVawcAts5Cgl9OMcqTA58+t9g61pMSOkSyKAVHn1qpvR2\nj9FeR5NxWlr1pAaWVyslkEGi2F+ypypaRa/lZ8iqAjn0eIVbcx+fRFz/5LAZ43Xr\ndyE8UHLdTMLFF+6CmdhxY5rxUenLeViIbbNHF/4pksscMyGsS0H8ZLoNAoGAR59r\nr4ge66RrB/ZO6xFMOjnaJ0vv5ALRESAMkkku1HdWeNEDT21yn5ywVTeYckbwgteq\nD/s9rcc8W6SkhxAM1fIzqV5D2LBxasro6PipfCPW8bkEEf3OPULUI8iyxvXNsh71\n5yMHyjr1OmiM6YTBq+X0vsRWL9ASJ6wB2elU5kECgYAranXHbi3JdVVppokoa35G\nk5OrfRhH87EVqLF8q+UyavF2Zsxd/I0lcU0fgtR0w23eZt2q6rx4nCeIKD3bm/ct\nxtxgvdG/GsqK/WuZbqa+my7bMoBjYNtN4lf+5l4ODQE7nCb8pd1iSYXZO1hfXIwg\nIMes/TRC6Gsz7K5AsgZgLg==\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-bmi70@checkgym-322d2.iam.gserviceaccount.com",
-  "client_id": "116016558862174674055",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-bmi70%40checkgym-322d2.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-})
+
 firebase_admin.initialize_app(cred, {'databaseURL': 'https://checkgym-322d2-default-rtdb.asia-southeast1.firebasedatabase.app/','storageBucket': 'checkgym-322d2.appspot.com'})
 ref = db.reference('users')  # 'users'는 사용자 데이터를 저장할 경로입니다.
 bucket = storage.bucket()
